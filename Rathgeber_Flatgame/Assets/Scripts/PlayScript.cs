0aea5cc526b0ffc78973a032802ec1aaa12da6d8
JacksonRathgeber <112485724+JacksonRathgeber@users.noreply.github.com>
2025-02-09 13:53:51 -0500

    final audio

--- Rathgeber_Flatgame/Assets/Scripts/PlayScript.cs
+++ Rathgeber_Flatgame/Assets/Scripts/PlayScript.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public bool is_active = false;
-    
+    public AudioMixer mixer;
+    public GameObject cam;
+
+    AudioSource[] AudioSources;
+
+    private bool is_moving = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        AudioSources = GetComponents<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,10 +26,37 @@ public class Player : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 this.transform.position += new Vector3(moveSpeed, 0, 0) * Time.deltaTime;
+                is_moving = true;
             }
             else if (Input.GetKey(KeyCode.A))
             {
                 this.transform.position += new Vector3(-moveSpeed, 0, 0) * Time.deltaTime;
+                is_moving = true;
+            }
+            else
+            {
+                is_moving = false;
+            }
+
+            if (is_moving)
+            {
+                mixer.SetFloat("player_volume",  -15);
+            }
+            else
+            {
+                mixer.SetFloat("player_volume", -80);
+            }
+            
+            for (int i = 0; i < AudioSources.Length; i++)
+            {
+                AudioSource a_s = AudioSources[i];
+                if (!a_s.isPlaying)
+                {
+                    if (Random.Range(0, 100) > 85)
+                    {
+                        a_s.Play();
+                    }
+                }
             }
         }
     }
